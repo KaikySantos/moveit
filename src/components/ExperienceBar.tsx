57d806474fe3c0d63c747b5e5e15a77a7f4a7096
KaikySantos <info.kaiky@gmail.com>
2021-02-23 17:09:16 -0300

    profile adicionado + images

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,11 +1,13 @@
+import styles from '../styles/components/ExperienceBar.module.css';
+
 export function ExperienceBar () {
     return (
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
                 <div style={{ width: '80%' }}></div>
 
-                <span className="current-experience" style={{ left: '80%' }}>
+                <span className={styles.currentExperience} style={{ left: '80%' }}>
                     300px
                 </span>
             </div>
